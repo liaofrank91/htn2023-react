06f8cab40c13f2cb7119060c711d0d095b6987ca
Frank Liao <liao.frank.91@gmail.com>
2023-09-17 00:40:11 -0400

    passing down setResultObj to CameraBox

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -9,7 +9,7 @@ function HomePage(props) {
         <b>SMART BITES</b>
       </div>
       <div style={{ height: "75%" }}>
-        <CameraBox preferences={props.preferences} />
+        <CameraBox preferences={props.preferences} setResultObj={props.setResultObj}/>
       </div>
       <div style={{ height: "15%", display: "flex", justifyContent: "center", flexDirection: "row", alignItems: "center"}}>
         <button type="button" onClick={() => props.switchToForm()} style={{width:"270px", height:"55px", fontSize:"18px", backgroundColor:"#2b4829", borderColor:"#2b4829", borderRadius:"10px", color:"#ecebc9"
